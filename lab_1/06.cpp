f3f49c2f1d1869a7c0d24958e433cc0cd510df2d
sukhovitskaia <71256021+sukhovitskaia@users.noreply.github.com>
2021-02-07 15:34:34 +0300

    Add files via upload

---
+++ lab_1/06.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    cout << "Your N:";
+    int N;
+    int n;
+    cin >> N;
+    n = floor((N - 1) / 2);
+    for (int i = 0; i <= n; i++) {
+        for (int j = 1; j <= i; j++)
+            cout << " ";
+        for (int j = i + 1; j <= N - i; j++)
+            cout << "*";
+        cout << std::endl;
+    }
+}
